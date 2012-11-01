9271e0516f4bc744cd1ab4ad625a7a7f354c8796
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-11-01 11:00:02 -0300

    se agrega el proyecto vacío

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'fd6558490910521fa19f5492697691d3e07d3676f413b9321084b5345bc336245594b30c00dd9fbbbc03cdf454d6e9abcc492e76c141eecd2ca85b4340d1c95d'
