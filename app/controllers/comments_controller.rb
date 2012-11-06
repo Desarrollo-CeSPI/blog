fbdd88feee215ddd40d5a4132793e2b47655b7b2
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-11-06 12:48:13 -0300

    pull request virtual

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,15 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = @post.comments.new(params[:comment])
+    @comment = Comment.new(params[:comment])
+    @comment.post = @post
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @post, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { render action: "../posts/show" }
+        format.html { render "posts/show" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
