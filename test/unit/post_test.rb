be02eacdeb8b146fad8ba5fa22eb34a679ed4722
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-11-06 09:42:38 -0300

    adding post tests

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'present attributes' do
+    post = Post.new
+    
+    assert post.invalid?
+    assert_equal 2, post.errors.size
+    assert_present post.errors[:content]
+    assert_present post.errors[:title]
+  end
+  
+  test 'unique attributes' do
+    post = Post.new
+    post.content = posts(:one).content
+    post.title = posts(:one).title
+
+    assert post.invalid?
+    assert_equal 1, post.errors.size
+    assert_present post.errors[:title]
+  end
 end
