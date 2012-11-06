c39fa0e4fb116e8ea1f6bd0986e0c41197c5369c
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-11-06 10:02:29 -0300

    asd

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,22 +1,4 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  test 'present attributes' do
-    post = Post.new
-    
-    assert post.invalid?
-    assert_equal 2, post.errors.size
-    assert_present post.errors[:content]
-    assert_present post.errors[:title]
-  end
-  
-  test 'unique attributes' do
-    post = Post.new
-    post.content = posts(:one).content
-    post.title = posts(:one).title
-
-    assert post.invalid?
-    assert_equal 1, post.errors.size
-    assert_present post.errors[:title]
-  end
 end
